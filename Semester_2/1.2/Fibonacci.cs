410dfe7caa2306562e5f6401c72e069866e91f9d
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-02-18 18:14:42 +0300

    1.2 fixed

--- Semester_2/1.2/Fibonacci.cs
+++ Semester_2/1.2/Fibonacci.cs
@@ -2,44 +2,23 @@ using System;
 
 namespace Fibonacci
 {
-    class Fibonacci
-    {
-        static public void Main()
-        {
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Exponentiation(new Matrix(), n).getFirst());
-        }
-        static private Matrix Exponentiation(Matrix a, int exp)
-        {
-            Matrix result = new Matrix();
-            for (int i = exp; i > 0; i >>= 1)
-            {
-                if (i % 2 != 0)
-                {
-                    result = result * a;
-                }
-                a = a * a;
-            }
-            return result;
-        }
-    }
     class Matrix
     {
-        private int[,] MA; //MatrixArray - in * this alias will be usefull
+        private int[,] mA; //matrixArray - in * this alias will be usefull
 
         public Matrix()
         {
-            MA = new int[2, 2] {{1, 1}, {1, 0}};
+            mA = new int[2, 2] {{1, 1}, {1, 0}};
         }
 
         public Matrix(int i1, int i2, int i3, int i4)
         {
-            MA = new int[2, 2] {{i1, i2}, {i3, i4}};
+            mA = new int[2, 2] {{i1, i2}, {i3, i4}};
         }
 
         public void print()
         {
-            foreach(int el in MA)
+            foreach(int el in mA)
             {
                 Console.Write(el);
             }
@@ -48,15 +27,15 @@ namespace Fibonacci
 
         public int getFirst()
         {
-            return MA[0,0];
+            return mA[0, 0];
         }
 
         public static Matrix operator*(Matrix m1, Matrix m2)
         {
-            return new Matrix(m1.MA[0,0] * m2.MA[0,0] + m1.MA[0,1] * m2.MA[1,0],
-                                        m1.MA[0,0] * m2.MA[0,1] + m1.MA[0,1] * m2.MA[1,1],
-                                        m1.MA[1,0] * m2.MA[0,0] + m1.MA[1,1] * m2.MA[1,0],
-                                        m1.MA[1,0] * m2.MA[0,1] + m1.MA[1,1] * m2.MA[1,1]);
+            return new Matrix(m1.mA[0, 0] * m2.mA[0, 0] + m1.mA[0, 1] * m2.mA[1, 0],
+                                        m1.mA[0, 0] * m2.mA[0, 1] + m1.mA[0, 1] * m2.mA[1, 1],
+                                        m1.mA[1, 0] * m2.mA[0, 0] + m1.mA[1, 1] * m2.mA[1, 0],
+                                        m1.mA[1, 0] * m2.mA[0, 1] + m1.mA[1, 1] * m2.mA[1, 1]);
         }
     }
 }
