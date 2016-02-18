2687affe5fb6c6abb576be8921a659a86b298cb9
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-02-18 12:06:17 +0300

    1.4 added

---
+++ Semester_2/1.4/Spiral.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Spiral
+{
+    class Spiral
+    {
+        public static void Main()
+        {
+            int [,] matrix = new int[5, 5] 
+            {{1,   2,   3,   4,  5},
+              {6,   7,   8,   9,  10},
+              {11, 12, 13, 14, 15},
+              {16, 17, 18, 19, 20},
+              {21, 22, 23, 24, 25}};
+              printFromTheInside(matrix);
+
+        }
+        private static void printFromTheInside(int[,] matrix)
+        {
+            int center = matrix.GetLength(0) / 2;
+            int i = center;
+            int j = center;
+            Console.Write(matrix[i, j]);
+            Console.Write(' ');
+            for (int step = 1; step <= matrix.GetLength(0) / 2; ++step)
+            {
+                ++i;
+                ++j;
+                while (j > center - step)
+                {
+                    --j;
+                    Console.Write(matrix[i, j]);
+                    Console.Write(' ');
+                }
+                while (i > center - step)
+                {
+                    --i;
+                    Console.Write(matrix[i, j]);
+                    Console.Write(' ');
+                }
+                while (j < center + step)
+                {
+                    ++j;
+                    Console.Write(matrix[i, j]);
+                    Console.Write(' ');
+                }
+                while (i < center + step)
+                {
+                    ++i;
+                    Console.Write(matrix[i, j]);
+                    Console.Write(' ');
+                }
+            }
+        }
+    }
+}
