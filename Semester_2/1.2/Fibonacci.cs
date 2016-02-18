bd6f1ba3053627434c1bc99ba2b09db3e4cf8c6d
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-02-18 18:19:47 +0300

    1.2 fixed

--- Semester_2/1.2/Fibonacci.cs
+++ Semester_2/1.2/Fibonacci.cs
@@ -6,17 +6,20 @@ namespace Fibonacci
     {
         private int[,] mA; //matrixArray - in * this alias will be usefull
 
+        ///creats standart matrix 2*2
         public Matrix()
         {
             mA = new int[2, 2] {{1, 1}, {1, 0}};
         }
 
+        ///takes four integers and creats matrix of them
         public Matrix(int i1, int i2, int i3, int i4)
         {
             mA = new int[2, 2] {{i1, i2}, {i3, i4}};
         }
 
-        public void print()
+        ///prints Matrix as its impossible to do from outside of the class
+        public void Print()
         {
             foreach(int el in mA)
             {
@@ -25,11 +28,13 @@ namespace Fibonacci
             Console.WriteLine();
         }
 
-        public int getFirst()
+        ///returns first element of Matrix
+        public int GetFirst()
         {
             return mA[0, 0];
         }
 
+        ///standart multiplation for matrix 2*2
         public static Matrix operator*(Matrix m1, Matrix m2)
         {
             return new Matrix(m1.mA[0, 0] * m2.mA[0, 0] + m1.mA[0, 1] * m2.mA[1, 0],
