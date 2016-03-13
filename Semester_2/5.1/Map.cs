41ee8c6c486470635f1a4404ee0acd6e40250b9e
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-03-13 18:48:32 +0300

    Idealogical Changes.

--- Semester_2/5.1/Map.cs
+++ Semester_2/5.1/Map.cs
@@ -7,11 +7,12 @@ namespace Map
     {
         static public List<int> Map(List<int> list, Func<int, int> f)
         {
+            List<int> result = new List<int>();
             for (int i = 0; i < list.Count; ++i)
             {
-                list[i] = f(list[i]);
+                result.Add(f(list[i]));
             }
-            return list;
+            return result;
         }
     }
 }
