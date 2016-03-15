73a8f96765c936e4580a05bd75cbe96336934ce1
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-03-15 20:26:26 +0300

    5.2 comments added.

--- Semester_2/5.2/Filter.cs
+++ Semester_2/5.2/Filter.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 
 namespace Filter
 {
+    /// A static class with one static method that returns list as a result.
     public static class FilterClass
     {
+        /// Takes List<int> and Func<int, bool> and returns
+        /// a list of elements, that returned true from the Func.
         static public List<int> Filter(List<int> list, Func<int, bool> f)
         {
             List<int> result = new List<int>();
