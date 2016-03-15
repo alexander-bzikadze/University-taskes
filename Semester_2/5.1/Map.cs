0669e80987c4638639286fb5d53036a03a038e97
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-03-15 20:23:38 +0300

    5.1 comments added.

--- Semester_2/5.1/Map.cs
+++ Semester_2/5.1/Map.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 
 namespace Map
 {
+    /// Static class with one static method that returs list.
     public static class MapClass
     {
+        /// Takes List<int> and Func<int, int> and returns list<int>, that is
+        /// element[i] = f(list[i]) for any i.
         static public List<int> Map(List<int> list, Func<int, int> f)
         {
             List<int> result = new List<int>();
