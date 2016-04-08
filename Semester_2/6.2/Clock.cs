d3d8d2999d18667ee3779d5c8797852f0dc8673c
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-04-09 00:35:33 +0300

    Clocks fixed.

--- Semester_2/6.2/Clock.cs
+++ Semester_2/6.2/Clock.cs
@@ -1,41 +1,50 @@
-using System;
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Windows;
 
 namespace Clocks
 {
-    public class Clocks : Form
-    {
-        private TableLayoutPanel space = null;
-        private Label label = null;
-        // private Timer timer = null;
+	public class Clocks : Form
+	{
+		private TableLayoutPanel space = null;
+		private Label label = null;
+		// private Timer timer = null;
 
-        public Clocks()
-        {
-            DisplayGUI();
-        }
+		public Clocks()
+		{
+			DisplayGUI();
+		}
 
-        private void DisplayGUI()
-        {
-            this.Size = new Size(SystemInformation.VirtualScreen.Width / 13,
-                                           SystemInformation.VirtualScreen.Height / 14);
+		private void DisplayGUI()
+		{
+			this.Size = new Size(SystemInformation.VirtualScreen.Width / 13,
+				SystemInformation.VirtualScreen.Height / 14);
 
-            label = new Label();
-            label.Text = DateTime.Now.ToString();
+			label = new Label();
+			label.Size = this.Size;
+			label.Text = DateTime.Now.ToString();
+			label.Font = new Font("Arial", this.Size.Width / 18,FontStyle.Bold);
 
-            space = new TableLayoutPanel();
-            space.Controls.Add(label);
-            this.Controls.Add(space);
+			space = new TableLayoutPanel();
+			space.Controls.Add(label);
+			this.Controls.Add(space);
 
-            var timer = new Timer();
-            timer.Start();
-            timer.Tick += ChangeTime;
-        }
+			var timer = new Timer();
+			timer.Start();
+			timer.Tick += ChangeTime;
+			this.Resize += ChangeFontSize;
+		}
 
-        private void ChangeTime(object sender, EventArgs args)
-        {
-            label.Text = DateTime.Now.ToString();
-        }
-    }
+		private void ChangeTime(object sender, EventArgs args)
+		{
+			label.Text = DateTime.Now.ToString();
+		}
+
+		private void ChangeFontSize(object sender, EventArgs args)
+		{
+			label.Size = this.Size;
+			label.Font = new Font("Arial", this.Size.Width / 18,FontStyle.Bold);
+		}
+	}
 }
---
+++ Semester_2/Clocks/Clocks/Clocks/Clocks.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clocks
+{
+	public class Clocks
+	{
+		public Clocks ()
+		{
+		}
+	}
+}
+
