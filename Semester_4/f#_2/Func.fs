d8e470856b50e1367f7ade0b36d46ba45ae6feb3
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2017-03-06 23:03:41 +0300

    Revision. Goodbye my sweet '_'.

--- Semester_4/f#_2/Func.fs
+++ Semester_4/f#_2/Func.fs
@@ -3,40 +3,38 @@ module Func
 open System;
 open System.Numerics;
 
-let rec sum_n_count n = 
+let rec prod n = 
     match n with
         | _ when 0 <= n && n < 10 -> n
-        | _ when n < 0 -> - sum_n_count (- n)
-        | _ -> (n % 10) * sum_n_count (n / 10)
+        | _ when n < 0 -> -prod (- n)
+        | _ -> (n % 10) * prod (n / 10)
 
-let first_pos x xs =
-    if List.length <| List.filter (fun a -> a = x) xs > 0 then
-        let rec helper x xs = 
-            match xs with
-                | [] -> 0
-                | a::tail -> if (a = x) then 0 else 1 + helper x tail
-        helper x xs
-    else
-        -1
+let firstPos x xs =
+    let rec helper x xs (index : int) = 
+        match xs with
+            | [] -> None
+            | a::tail when a = x -> Some(index)
+            | a::tail -> helper x tail (index + 1)
+    helper x xs 0
 
-let pal xs =
+let pal (str : String) =
+    let xs = [for c in str -> c]
     List.rev xs = xs
 
 
-let rec mergesort xs =
-    let rec merge : int list * int list -> int list = 
-        fun (ax, bx) ->
-            match (ax, bx) with
-                | [], [] -> [] 
-                | _, [] -> ax
-                | [], _ -> bx
-                | a::atail, b::btail when a < b -> a :: merge (atail, bx)
-                | a::atail, b::btail -> b :: merge (ax, btail)
+let rec mergeSort xs =
+    let rec merge ax bx acc = 
+        match (ax, bx) with
+            | [], [] -> List.rev acc
+            | _, [] -> (List.rev acc) @ ax
+            | [], _ -> (List.rev acc) @ bx
+            | a::atail, b::btail when a < b -> merge atail bx (a :: acc)
+            | a::atail, b::btail -> merge ax btail (b :: acc)
     match xs with
     | [] -> []
     | [a] -> [a]
     | _ -> 
         let a, b = List.splitAt (List.length xs / 2) xs
-        merge (mergesort a, mergesort b)
+        merge (mergeSort a) (mergeSort b) []
 
-let mergesort_2 = List.sortBy (fun elem -> abs elem)
+let mergeSort_2 = List.sortBy (fun elem -> abs elem)
