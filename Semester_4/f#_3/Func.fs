d2d5a55e4f85532651c93e462bffc9925da07489
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2017-03-12 22:56:37 +0300

    Oh, oh, sweet '_' of mine.

--- Semester_4/f#_3/Func.fs
+++ Semester_4/f#_3/Func.fs
@@ -1,14 +1,14 @@
 module Func
 
 // For ordered. I guess there is no way to do it not in O(n^2) without comparison as there is no order
-let is_unique l = 
+let isUnique l = 
   let rec helper l = 
     match l with
       | x :: y :: xs -> if x = y then false else helper (y :: xs)
       | _ -> true
   helper (List.sort l) // tried >> but it went wrong as List.sort is generic. Aid?
 
-let max_neighbour l = 
+let maxNeighbour l = 
   let rec helper l acc =
     match l with
       | x :: y :: xs -> helper (y :: xs) (max acc (x + y))
